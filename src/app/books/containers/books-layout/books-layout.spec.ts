689f7dec747d024e792f28d4560b32ec6af7bfed
Armin <arminmow@gmail.com>
2025-09-07 12:23:26 +0330

    Add modal for adding new books to layout
    Introduces a generic modal with an add book form to the books layout template. Updates the spec file with stubs for the modal, form, and button directive to support testing.

--- src/app/books/containers/books-layout/books-layout.spec.ts
+++ src/app/books/containers/books-layout/books-layout.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BooksLayout } from './books-layout';
-import { Component } from '@angular/core';
+import { Component, Directive, Input } from '@angular/core';
 
 @Component({
   selector: 'app-book-table',
@@ -10,13 +10,41 @@ import { Component } from '@angular/core';
 })
 class BookTableStub {}
 
+@Component({
+  selector: 'app-generic-modal',
+  template: '',
+  standalone: false,
+})
+class GenericModalStub {}
+
+@Component({
+  selector: 'app-add-book-form',
+  template: '',
+  standalone: false,
+})
+class AddBookFormStub {}
+
+@Directive({
+  selector: '[nz-button]',
+  standalone: false,
+})
+class NzButtonStubDirective {
+  @Input() nzType: string | undefined;
+}
+
 describe('BooksLayout', () => {
   let component: BooksLayout;
   let fixture: ComponentFixture<BooksLayout>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [BooksLayout, BookTableStub],
+      declarations: [
+        BooksLayout,
+        BookTableStub,
+        GenericModalStub,
+        AddBookFormStub,
+        NzButtonStubDirective
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(BooksLayout);
