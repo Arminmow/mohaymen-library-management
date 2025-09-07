885efc262f7c73f951cd2c5f6625751457af1c95
Armin <arminmow@gmail.com>
2025-09-07 13:27:56 +0330

    Move persistenceService into shared folder
    Moved persistenceService from userModule to shared folder since its a singleton service and implemented in book store

--- src/app/books/stores/book-store.ts
+++ src/app/books/stores/book-store.ts
@@ -1,5 +1,7 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 import { ComponentStore } from '@ngrx/component-store';
+import { PersistenceService } from '../../shared/services/persistence-service/persistence-service';
+import { Subscription } from 'rxjs';
 
 export interface Book {
   id: number;
@@ -12,20 +14,30 @@ export interface BookState {
   books: Book[];
 }
 
+const STORAGE_KEY = 'books-state';
+
 @Injectable({
   providedIn: 'root',
 })
-export class BookStore extends ComponentStore<BookState> {
-  constructor() {
-    super({
-      books: [
-        {
-          id: 1,
-          title: 'harry potter and the sorcerers stone',
-          author: 'J.K. Rowling',
-          publishedDate: new Date('1997-06-26'),
-        },
-      ],
+export class BookStore extends ComponentStore<BookState> implements OnDestroy {
+  private subscription: Subscription;
+
+  constructor(private persistenceService: PersistenceService) {
+    super(
+      persistenceService.get<BookState>(STORAGE_KEY) ?? {
+        books: [
+          {
+            id: 1,
+            title: 'harry potter and the sorcerers stone',
+            author: 'J.K. Rowling',
+            publishedDate: new Date('1997-06-26'),
+          },
+        ],
+      }
+    );
+
+    this.subscription = this.books$.subscribe((books) => {
+      this.persistenceService.save(STORAGE_KEY, { books });
     });
   }
 
@@ -45,4 +57,8 @@ export class BookStore extends ComponentStore<BookState> {
     ...state,
     books: state.books.map((b) => (b.id === book.id ? book : b)),
   }));
+
+  override ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
