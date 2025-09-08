219c2077fe3c2dd6d7dff203c7cc7d55b3ac6ec3
Armin <arminmow@gmail.com>
2025-09-08 13:09:16 +0330

    replace @Input with reactive store contextBook$

--- src/app/books/components/book-actions/book-actions.ts
+++ src/app/books/components/book-actions/book-actions.ts
@@ -1,5 +1,5 @@
-import { Component, Input, ViewChild } from '@angular/core';
-import { Book } from '../../stores/book-store';
+import { Component, ViewChild } from '@angular/core';
+import { Book, BookStore } from '../../stores/book-store';
 import { NzDropdownMenuComponent } from 'ng-zorro-antd/dropdown';
 import { BookService } from '../../services/book-service';
 
@@ -10,18 +10,12 @@ import { BookService } from '../../services/book-service';
   styleUrl: './book-actions.scss',
 })
 export class BookActions {
-  @Input() contextBook!: Book | null;
-
   @ViewChild('menu', { static: true, read: NzDropdownMenuComponent })
   public menu!: NzDropdownMenuComponent;
 
+  constructor(private bookService: BookService, public bookStore: BookStore) {}
 
-  constructor(private bookService: BookService) {}
-
-  showDeleteConfirm() {
-    if (this.contextBook) {
-      this.bookService.confirmDelete(this.contextBook);
-    }
+  showDeleteConfirm(book: Book) {
+    this.bookService.confirmDelete(book);
   }
-
 }
