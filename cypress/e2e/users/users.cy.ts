aef095fc9ad92572185f7021198c760482d89336
Armin <arminmow@gmail.com>
2025-09-09 12:57:26 +0330

    add users table rendering tests

--- cypress/e2e/users/users.cy.ts
+++ cypress/e2e/users/users.cy.ts
@@ -1,6 +1,6 @@
 describe('Users Module (integration)', () => {
   beforeEach(() => {
-    cy.visit('/users'); 
+    cy.visit('/users');
   });
 
   it('should display Users Table title', () => {
@@ -14,5 +14,15 @@ describe('Users Module (integration)', () => {
   it('should open Add User modal when button is clicked', () => {
     cy.contains('Add User').click();
     cy.get('app-add-user-form').should('exist');
+  });
+
+  it('should display the users table with headers', () => {
+    cy.get('nz-table thead th').eq(0).should('contain.text', 'Name');
+    cy.get('nz-table thead th').eq(1).should('contain.text', 'Age');
+    cy.get('nz-table thead th').eq(2).should('contain.text', 'Role');
+  });
+
+  it('should display at least one row in the table', () => {
+    cy.get('nz-table tbody tr').its('length').should('be.gte', 1);
   });
 });
